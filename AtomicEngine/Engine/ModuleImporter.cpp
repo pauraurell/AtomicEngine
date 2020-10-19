d431c911e81f204cc0462534ebee454c1cdc3418
pgalmor <47526823+PoleteTomaPedete@users.noreply.github.com>
2020-10-19 21:01:15 +0200

    fbx load wip

--- AtomicEngine/Engine/ModuleImporter.cpp
+++ AtomicEngine/Engine/ModuleImporter.cpp
@@ -43,3 +43,37 @@ bool ModuleImporter::CleanUp() {
 	return true;
 }
 
+void ModuleImporter::LoadMesh(char* file_path)
+{
+	const aiScene* scene = aiImportFile(file_path, aiProcessPreset_TargetRealtime_MaxQuality);
+	if (scene != nullptr && scene->HasMeshes())
+	{
+		for (int i = 0; i > scene->mNumMeshes; i++)
+		{
+			aiMesh *ourMesh = scene->mMeshes[i];
+			myMesh.num_vertex = ourMesh->mNumVertices;
+			myMesh.vertex = new float[myMesh.num_vertex * 3];
+			memcpy(myMesh.vertex, ourMesh->mVertices, sizeof(float) * myMesh.num_vertex * 3);
+			LOG("New mesh with %d vertices", myMesh.num_vertex);
+
+			// copy faces
+			if (ourMesh->HasFaces())
+			{
+				myMesh.num_index = ourMesh->mNumFaces * 3;
+				myMesh.index = new uint[myMesh.num_index]; // assume each face is a triangle
+				for (uint i = 0; i < ourMesh->mNumFaces; ++i)
+				{
+					if (ourMesh->mFaces[i].mNumIndices != 3)
+					{ LOG("WARNING, geometry face with != 3 indices!"); }
+					else
+					{ memcpy(&myMesh.index[i * 3], ourMesh->mFaces[i].mIndices, 3 * sizeof(uint)); }
+				}
+			}
+		}
+
+		aiReleaseImport(scene);
+	}
+	else
+		LOG("Error loading scene %s", file_path);
+}
+
--- AtomicEngine/Engine/ModuleImporter.h
+++ AtomicEngine/Engine/ModuleImporter.h
@@ -11,4 +11,20 @@ public:
 	bool Init();
 	update_status Update();
 	bool CleanUp();
+
+	void LoadMesh(char* file_path);
+	
+	struct mesh
+	{
+		uint id_index = 0; // index in VRAM
+		uint num_index = 0;
+		uint* index = nullptr;
+
+		uint id_vertex = 0; // unique vertex in VRAM
+		uint num_vertex = 0;
+		float* vertex = nullptr;
+	};
+	mesh myMesh;
+
+
 };
